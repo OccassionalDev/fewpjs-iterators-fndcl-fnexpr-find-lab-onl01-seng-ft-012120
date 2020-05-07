153451be93784765a3c5377c94f036b32f223747
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-07 17:00:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+];
 
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+  let result = record.find(record => record.result === "W");
+  
+  if (result !== undefined) {
+    return result.year;
+  }
+  
+  else {
+    return undefined;
+  }
 }
