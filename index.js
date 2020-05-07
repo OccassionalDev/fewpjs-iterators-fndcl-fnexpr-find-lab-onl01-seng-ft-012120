81d7ce1c0c8ca256915accc73dc0064119db251d
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-07 16:55:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const testVar = {};
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+];
 
-function testFunc() {
+function superbowlWin(team) {
   return "hi";
 }
